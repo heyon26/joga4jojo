7cc73b14c9907bbf33f2c577a3851cca3642e384
songys <yellowsys92@naver.com>
2021-02-15 11:46:52 +0900

    클래스 이미지 추가, 버튼 색 수정 완료 (2021/02/15 11:46)

--- MochaKmong/src/co/mok/pro/common/DAO.java
+++ MochaKmong/src/co/mok/pro/common/DAO.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DAO {
 	private String driver = "oracle.jdbc.driver.OracleDriver";
 
-	private String url = "jdbc:oracle:thin:@39.116.34.40:1521:orcl"; /* 집에서 db연결시 */
-	/* private String url ="jdbc:oracle:thin:@192.168.0.87:1521:xe"; */
+	/* private String url = "jdbc:oracle:thin:@39.116.34.40:1521:orcl"; */ /* 집에서 db연결시 */
+	private String url ="jdbc:oracle:thin:@192.168.0.87:1521:xe";
 
 	private String user ="mokapro";
 	private String password = "1234";
